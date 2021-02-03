c80da86f3068c773522517c7e10c01f03c29a994
Sahil Naikdhure <sanaikdhure@gmail.com>
2021-02-03 18:07:30 +0530

    add individual update methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function MyApp() {
+
+    const [applesCount, setApplesCount] = useState(0);
+    const [kiwiCount, setKiwisCount] = useState(0);
+    const [orangeCount, setOrangessCount] = useState(0);
+
+    const totalFcount = applesCount + kiwiCount + orangeCount;
+
+    function addAppleToBasket() {
+        setApplesCount(applesCount + 1);
+    }
+
+    function removeAppleFromBasket() {
+        setApplesCount(applesCount - 1);
+    }
+
+    function addKiwiToBasket() {
+        setKiwisCount(kiwiCount + 1);
+    }
+
+    function removeKiwiFromBasket() {
+        setKiwisCount(kiwiCount -1);
+    }
+
+    function addOrangeToBasket() {
+        setOrangessCount(orangeCount +1);
+    }
+
+    function removeOrangeFromBasket() {
+        setOrangessCount(orangeCount -1);
+    }
+
+    return (
+    <div>
+        <h1>Fruit Shop</h1>
+        <div>
+            Apples
+            <button onClick={addAppleToBasket} >+</button>
+            {applesCount}
+            <button onClick={removeAppleFromBasket} >-</button>
+        </div>
+
+        <div>
+            Kiwi
+            <button onClick={addKiwiToBasket} >+</button>
+            {kiwiCount}
+            <button onClick={removeKiwiFromBasket} >-</button>
+        </div>
+
+        <div>
+            Orange
+            <button onClick={addOrangeToBasket} >+</button>
+            {orangeCount}
+            <button onClick={removeOrangeFromBasket} >-</button>
+        </div>
+        
+        <hr/>
+        <h1>Invoice</h1>
+
+        <div>Total Fruits Added: {totalFcount}</div>
+        <div>Total Price: 100</div>
+        <div>Tax %: 5</div>
+        <div>Tax Price: 5</div>
+        <br/>
+        <div>Total Price: 105</div>
+    </div>)
 }
 
-export default App;
+export default MyApp;
+
+
+// Fruit Shop app
+
+// Buying few fruits
+// pay the bill
+// generate an invoice
+// invoice content : number of fruits, price, tax %
